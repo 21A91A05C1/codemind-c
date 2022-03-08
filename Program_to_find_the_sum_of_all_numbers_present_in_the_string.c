5a9776ac5c03ef1684f77afb2e0a74df8d440de4
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-03-08 22:00:54 +0530

    #include<stdio.h> #include<ctype.h> int main() {     int i,sum=0;     char str[1000];     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='0' && str[i]<='9')         {             sum=sum+str[i]-'0';         }
        }
        printf("%d",sum);
    }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    int i,sum=0;
+    char str[1000];
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='0' && str[i]<='9')
+        {
+            sum=sum+str[i]-'0';
+        }
+    
+    }
+    printf("%d",sum);
+}
