d42457bdba1a0ac68ba2fe92ebeaaa40784da8aa
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-04-12 21:55:58 +0530

    #include<stdio.h> int main() { 	int n,i,flag=0,arr[100],min=9999,j; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=0;i<n;i++) 	{ 		if(min>arr[i]) 		{ 			min=arr[i]; 		} 	} 	for(i=min;i>=1;i--) 	{ 		flag=0; 		for(j=0;j<n;j++) 		{ 			if(arr[j]%i!=0) 			{ 				flag=1; 				break; 			} 		} 		if(flag==0) 		{ 			printf("%d ",i); 			break; 		} 	} 	return 0;
    }

---
+++ Minimum_Scale_length.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,flag=0,arr[100],min=9999,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(min>arr[i])
+		{
+			min=arr[i];
+		}
+	}
+	for(i=min;i>=1;i--)
+	{
+		flag=0;
+		for(j=0;j<n;j++)
+		{
+			if(arr[j]%i!=0)
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			printf("%d ",i);
+			break;
+		}
+	}
+	return 0;
+	
+}
