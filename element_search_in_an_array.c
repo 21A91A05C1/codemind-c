e056672e2d73ae7d5935384cd831d47cfab771ff
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-04-15 21:32:30 +0530

    #include<stdio.h> int main() {     int n,k,arr[100],i,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)         scanf("%d",&arr[i]);         scanf("%d",&k);      for(i=0;i<n;i++)      {          if(k==arr[i])          {              flag=1;          }
         }
         if(flag==1)
            printf("True");
            else
                printf("False");
    }

---
+++ element_search_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,arr[100],i,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+        scanf("%d",&k);
+     for(i=0;i<n;i++)
+     {
+         if(k==arr[i])
+         {
+             flag=1;
+         }
+         
+     }
+     if(flag==1)
+        printf("True");
+        else
+            printf("False");
+}
