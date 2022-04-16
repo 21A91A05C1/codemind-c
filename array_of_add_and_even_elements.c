d39a4f9892c558698251f407a0965c01fcd26541
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-04-16 11:17:26 +0530

    #include<stdio.h> int main() {     int n,i,odd[1000],even[1000],arr[1000],o=0,e=0,co=0,ce=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             o++;             odd[co]=arr[i];             co++;         }         else         {             e++;             even[ce]=arr[i];             ce++;         }     }     for(co=0;co<o;co++)     {         printf("%d ",odd[co]);     }     for(ce=0;ce<e;ce++)     {         printf("%d ",even[ce]);     } }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,odd[1000],even[1000],arr[1000],o=0,e=0,co=0,ce=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            o++;
+            odd[co]=arr[i];
+            co++;
+        }
+        else
+        {
+            e++;
+            even[ce]=arr[i];
+            ce++;
+        }
+    }
+    for(co=0;co<o;co++)
+    {
+        printf("%d ",odd[co]);
+    }
+    for(ce=0;ce<e;ce++)
+    {
+        printf("%d ",even[ce]);
+    }
+}
