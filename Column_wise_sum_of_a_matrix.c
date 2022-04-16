ba504db6fc7aeb637f87e89536ec859a262fe47c
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-04-16 11:53:54 +0530

    #include<stdio.h> int main() {     int r,c,i,j,arr[1000][1000],sum =0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(j=0;j<c;j++)     { // max=-9999;     sum=0;         for(i=0;i<r;i++)         {
                    sum=sum+arr[i][j];
            }
            printf("%d ",sum);
        }
    }

---
+++ Column_wise_sum_of_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,arr[1000][1000],sum =0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(j=0;j<c;j++)
+    { // max=-9999;
+    sum=0;
+        for(i=0;i<r;i++)
+        {  
+            
+                sum=sum+arr[i][j];
+        }
+        printf("%d ",sum);
+    }
+}
