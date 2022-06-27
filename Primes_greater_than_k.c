1ef00460fd07ab48ba5788b34a55cc8923aa2107
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:20:36 +0530

    #include<stdio.h> int main() {     int arr[1000],i,n,c=0,d=0,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int k;     scanf("%d",&k);     for(i=0;i<n;i++)     {c=0;     if(arr[i]==1)     continue;         for(j=2;j<arr[i];j++)         {             if(arr[i]%j==0)             {                 c++;             }         }         if(c==0)         {             if(arr[i]>=k)             d++;         }     }     printf("%d",d); }

--- Primes_greater_than_k.c
+++ Primes_greater_than_k.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,arr[100],j,d=0,c=0;
+    int arr[1000],i,n,c=0,d=0,j;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -10,16 +10,21 @@ int main()
     int k;
     scanf("%d",&k);
     for(i=0;i<n;i++)
-    { c=0;
-        if(arr[i]==1)
-        continue;
+    {c=0;
+    if(arr[i]==1)
+    continue;
         for(j=2;j<arr[i];j++)
         {
             if(arr[i]%j==0)
-            c++;
+            {
+                c++;
+            }
         }
-        if(c==0 && arr[i]>=k)
+        if(c==0)
+        {
+            if(arr[i]>=k)
             d++;
+        }
     }
     printf("%d",d);
 }
