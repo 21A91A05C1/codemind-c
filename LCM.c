70560244e051b8b4fe99e9f26b5ec6bfd56ea9d5
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:13:48 +0530

    #include<stdio.h>  int main()  {      int num1, num2, i, gcd, lcm;
         scanf("%d %d", &num1, &num2);
    
         for(i=1; i<=num1 && i<=num2; i++)
         {
             if(num1%i==0 && num2%i==0)
                 gcd=i;
         }
    
         lcm=(num1*num2)/gcd;
    
         printf("%d",lcm);
    
         return 0;
     }

--- LCM.c
+++ LCM.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
-int main()
-{
-    int n,m,res,i,lcm;
-    scanf("%d%d",&n,&m);
-    for(i=1;i<=n && i<=m;i++)
-    {
-        if(n%i==0 && m%i==0)
-        res=i;
-    }
-    lcm=n*m/res;
-    printf("%d",lcm);
-    
-}
+ int main()
+ {
+     int num1, num2, i, gcd, lcm;
+
+
+     scanf("%d %d", &num1, &num2);
+
+     for(i=1; i<=num1 && i<=num2; i++)
+     {
+         if(num1%i==0 && num2%i==0)
+             gcd=i;
+     }
+
+     lcm=(num1*num2)/gcd;
+
+     printf("%d",lcm);
+
+     return 0;
+ }
