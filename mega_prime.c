f711fef8112a9ce558c21920b43c58054f31aa1b
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:33:23 +0530

    #include<stdio.h> int prime(int n) {     int c=0,i;     for(i=1;i<=n;i++)     {
        if(n%i==0)
        {
            c++;
        }
        }
        return c;
    }
    int main()
    {
        int n,i,c,d,s=0,k=0,z=0;
        scanf("%d",&n);
        c=prime(n);
        if(c==2)
        {
            while(n>0)
            {k=0;
                d=n%10;
                s++;
                for(i=1;i<=d;i++)
                {
                    if(d%i==0)
                    {
                        k++;
                    }
                }
                n=n/10;
            
                if(k==2)
                {
                    z++;
                }
                
                
                
            }
            if(z==s)
            {
                printf("Mega Prime");
            }
            else
            {
                printf("Not Mega Prime");
            }
            
        }
        else
        {
            printf("Not Mega Prime");
        }
        
    }

--- mega_prime.c
+++ mega_prime.c
@@ -1,54 +1,58 @@
 #include<stdio.h>
-int isprime(int n)
+int prime(int n)
 {
-	int i,c=0;
-	for(i=1;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			c++;
-		}
-	}
-	return c;
+    int c=0,i;
+    for(i=1;i<=n;i++)
+    {
+    
+    if(n%i==0)
+    {
+        c++;
+    }
+    }
+    return c;
 }
-
 int main()
 {
-	int z=0,n,s=0,c,d,r,i,k=0;
-	scanf("%d",&n);
-	c=isprime(n);
-	if(c==2)
-	{
-		while(n!=0)
-		{//54
-		k=0;
-			d=n%10;//4 5
-			s++;
-			for(i=1;i<=d;i++)
-			{
-				if(d%i==0)
-				{
-					k++;
-				}
-			}
-			n=n/10;
-		
-		if(k==2)
-			{
-				z++;
-			}
-		}
-		if(s==z)
-		{
-			printf("Mega Prime");
-		}
-		else
-		{
-			printf("Not Mega Prime");
-		}
-	}
-	else
-	{
-		printf("Not Mega Prime");
-	}
+    int n,i,c,d,s=0,k=0,z=0;
+    scanf("%d",&n);
+    c=prime(n);
+    if(c==2)
+    {
+        while(n>0)
+        {k=0;
+            d=n%10;
+            s++;
+            for(i=1;i<=d;i++)
+            {
+                if(d%i==0)
+                {
+                    k++;
+                }
+            }
+            n=n/10;
+        
+            if(k==2)
+            {
+                z++;
+            }
+            
+            
+            
+        }
+        if(z==s)
+        {
+            printf("Mega Prime");
+        }
+        else
+        {
+            printf("Not Mega Prime");
+        }
+        
+    }
+    else
+    {
+        printf("Not Mega Prime");
+    }
+    
 }
