1c8990115a128aff23cd437ab72de3e8be3e2316
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:32:58 +0530

    #include<stdio.h> int main() {     int f1,f2,f3,n;     scanf("%d",&n);     f1=0;     f2=1;     f3=f1+f2;     while(f3<n)     {         f1=f2;         f2=f3;         f3=f1+f2;
        }
        if(f3==n)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

--- Fibonacci_or_not.c
+++ Fibonacci_or_not.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,f1=0,f2=1,f3;
+    int f1,f2,f3,n;
     scanf("%d",&n);
+    f1=0;
+    f2=1;
     f3=f1+f2;
     while(f3<n)
     {
         f1=f2;
         f2=f3;
         f3=f1+f2;
+        
     }
     if(f3==n)
     {
@@ -18,7 +21,4 @@ int main()
     {
         printf("False");
     }
-    
-    
-    
 }
