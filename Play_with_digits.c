0888858281f2cc00cd7c2518e976c1ae235e026b
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:04:20 +0530

    #include<stdio.h> int main() {     int arr[1000],n,i,sum=0,d;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         while(arr[i]>0)         {             d=arr[i]%10;             arr[i]=arr[i]/10;             sum=sum+d;         }
        }
        printf("%d",sum);
    }

--- Play_with_digits.c
+++ Play_with_digits.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int i,arr[1000],n,sum=0,d;
+    int arr[1000],n,i,sum=0,d;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
-    {  
-        while(arr[i])
+    {
+        while(arr[i]>0)
         {
             d=arr[i]%10;
             arr[i]=arr[i]/10;
             sum=sum+d;
         }
+        
     }
-   printf("%d",sum);
-    
+    printf("%d",sum);
 }
