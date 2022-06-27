a464d6871cefbd12133ea6c2b59221db1822ee32
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-06-27 23:41:56 +0530

    #include<stdio.h> int main() {     int N,d,c=0,lc=0;     scanf("%d",&N);//9876     while(N>0)     {     	d=N%10;//6        	N=N/10;//987        	c=d;        	if(lc<c)//6<7     	{         	lc=c;//7     	}     }     printf("%d",lc); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int N,lc=0,c=0,d;
-    scanf("%d",&N);
+    int N,d,c=0,lc=0;
+    scanf("%d",&N);//9876
     while(N>0)
     {
-        d=N%10;
-        N=N/10;
-        c=d;
-        if(lc<c)
-        {
-            lc=c;
-        }
+    	d=N%10;//6
+       	N=N/10;//987
+       	c=d;
+       	if(lc<c)//6<7
+    	{
+        	lc=c;//7
+    	}
     }
-    printf("%d",lc);
+    printf("%d",lc); 
 }
