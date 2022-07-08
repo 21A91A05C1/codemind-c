e36d24ab6a016f3221ca38c437e84104e2b22cb6
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-07-08 23:32:33 +0530

    #include<stdio.h> int main() {     char str[1000];     int i,n=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]=='a'|| str[i]=='e'|| str[i]=='i'|| str[i]=='o'|| str[i]=='u'         || str[i]=='A'|| str[i]=='E'|| str[i]=='I'|| str[i]=='O'|| str[i]=='U')         {             n++;         }     }     printf("%d",n); }

--- vowel_count.c
+++ vowel_count.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,c=0;
-    char str[100];
+    char str[1000];
+    int i,n=0;
     scanf("%[^
 ]s",str);
     for(i=0;str[i]!=NULL;i++)
     {
-        if(str[i]=='a'|| str[i]=='e'||str[i]=='i'||str[i]=='o'
-        || str[i]=='u' || str[i]=='A'|| str[i]=='E'||
-        str[i]=='O'||str[i]=='U')
+        if(str[i]=='a'|| str[i]=='e'|| str[i]=='i'|| str[i]=='o'|| str[i]=='u'
+        || str[i]=='A'|| str[i]=='E'|| str[i]=='I'|| str[i]=='O'|| str[i]=='U')
         {
-             c++;
+            n++;
         }
     }
-    printf("%d",c);
-    return 0;
+    printf("%d",n);
 }
