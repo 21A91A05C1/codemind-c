fbb736e9db6f9470b1ffefe8d7c1eeed53c2ad27
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-07-09 21:53:19 +0530

    #include<stdio.h> int main() {     int n,i,k1=0,k2=0,c=0;     scanf("%d",&n);     int arr[100];     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     for(i=1;i<n-1;i++)     {         if(arr[i-1]%2==1 &&  arr[i+1]%2==0|| arr[i-1]%2==0&& arr[i+1]%2==1)         {             c++;         }     }     printf("%d",c); }

--- count_numbers_between_even_and_odd_numbers_or_vice_versa.c
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d",&arr[i]);
     for(i=1;i<n-1;i++)
     {
-        if(arr[i-1]%2==0 && arr[i+1]%2==1 ||arr[i-1]%2==1 && arr[i+1]%2==0)
+        if(arr[i-1]%2==1 &&  arr[i+1]%2==0|| arr[i-1]%2==0&& arr[i+1]%2==1)
         {
             c++;
         }
