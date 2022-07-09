2efdc4858c4df7e63465d37e4e62119fc5950312
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-07-09 22:17:17 +0530

    #include<stdio.h> int main() {     int arr[1000],org=0,dn=0,b=1,i,r,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         org=org*10+arr[i];     }     while(org)     {         r=org%10;         dn=dn+(r*b);         org=org/10;         b=b*2;     }     printf("%d",dn); }

--- Binary_Array_to_decimal.c
+++ Binary_Array_to_decimal.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[1000],i,r,rev=0,org=0,dn=0,b=1;
+    int arr[1000],org=0,dn=0,b=1,i,r,n;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);//1 1 0 0
+        scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
     {
-        org=org*10+arr[i];//1100
+        org=org*10+arr[i];
     }
     while(org)
     {
-        r=org%10;//0 0 1 1
-        dn=dn+(r*b);//0 0 4 12
-        org=org/10;//110 11 1 0
-        b=b*2;//2 4 8
+        r=org%10;
+        dn=dn+(r*b);
+        org=org/10;
+        b=b*2;
     }
     printf("%d",dn);
 }
