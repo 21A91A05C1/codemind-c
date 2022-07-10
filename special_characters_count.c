b0cec3dbb9ca1016e0c266b6ff59409b8220e71d
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2022-07-10 11:54:04 +0530

    #include<stdio.h> int main() {     char str[1000];     int i,n=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=33 && str[i]<=47 || str[i]>=58         && str[i]<=64 || str[i]>=91 && str[i]<=96         || str[i]>=123 && str[i]<=126)         {             n++;         }     }     printf("%d",n); }

--- special_characters_count.c
+++ special_characters_count.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int i,c=0;
     char str[1000];
+    int i,n=0;
     scanf("%[^
 ]s",str);
     for(i=0;str[i]!=NULL;i++)
     {
-        if(str[i]>=33 && str[i]<=47 || str[i]>=91 && 
-        str[i]<=96|| str[i]>=123 && str[i]<=126
-        ||str[i]>=58 && str[i]<=64)
+        if(str[i]>=33 && str[i]<=47 || str[i]>=58
+        && str[i]<=64 || str[i]>=91 && str[i]<=96
+        || str[i]>=123 && str[i]<=126)
         {
-             c++;
+            n++;
         }
     }
-    printf("%d",c);
-    return 0;
+    printf("%d",n);
 }
