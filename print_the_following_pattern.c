8ed971c510d8debb9fe2bb1160290f109bbc5bb6
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2023-02-04 10:58:57 +0530

    #include<stdio.h> int main() {     int i , j ,k = 0 , l = 0;     int n;     scanf("%d",&n);     for(i = 0 ; i < n ; i++) 	{	int k=i; 		int l=0; 		for(j = 0 ; j < n-i-1 ; j++) 		{ 			//cout<<" "<<" "; 			printf(" "); 		} 		for(j = 0 ; j <i;j++) 		{ 			//cout<<k<<" "; 			printf("%d",k); 			k--; 		} 		for(j = 0 ; j <= i ; j++) 		{ 			//cout<<l<<" "; 			printf("%d",l); 			l++; 		}
    		printf("
    ");
    	}
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
 int main()
 {
-     
-     int i,n,j;
-     scanf("%d",&n);
-     for( i=1;i<=n;i++)
-     {
-         for(j=1;j<=n;j++)
-         {
-             if(i<n)
-             {
-                 if(j==1|| j==i)
-                 {
-                    printf("*");
-                 }
-                  else
-                  {
-                     printf(" ");
-                  }
-             }
-             if(i==n && j<=n)
-             {
-                printf("*");
-             }
-         }
-         printf("
+    int i , j ,k = 0 , l = 0;
+    int n;
+    scanf("%d",&n);
+    for(i = 0 ; i < n ; i++)
+	{	int k=i;
+		int l=0;
+		for(j = 0 ; j < n-i-1 ; j++)
+		{
+			//cout<<" "<<" ";
+			printf(" ");
+		}
+		for(j = 0 ; j <i;j++)
+		{
+			//cout<<k<<" ";
+			printf("%d",k);
+			k--;
+		}
+		for(j = 0 ; j <= i ; j++)
+		{
+			//cout<<l<<" ";
+			printf("%d",l);
+			l++;
+		}
+		
+		printf("
 ");
-     }
-     
+	}
 }
